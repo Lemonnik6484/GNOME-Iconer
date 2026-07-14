db2c0d841500a632a1e6a00fe3fb9f1541f8413f
lemonnik <lemonniik.0@gmail.com>
2026-07-14 04:50:03 +0200

    versioning fixes

--- src/main/java/dev/lemonnik/iconer/mixin/MinecraftMixin.java
+++ src/main/java/dev/lemonnik/iconer/mixin/MinecraftMixin.java
@@ -10,14 +10,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-    //? if >1.21.1 {
+    //? if >1.20.1 {
     @Inject(method = "<init>", at = @At("HEAD"))
     private static void iconer$init(CallbackInfo ci) {
         Iconer.genFiles();
     }
     //?}
 
-    //? if >1.21.1 {
+    //? if >1.21.11 {
     @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;<init>(Lcom/mojang/blaze3d/platform/WindowEventHandler;Lcom/mojang/blaze3d/platform/DisplayData;Ljava/lang/String;Ljava/lang/String;Lcom/mojang/blaze3d/systems/GpuBackend;)V"), index = 3)
     private static String iconer$swapWindowName(String original) {
         return "Minecraft*";
